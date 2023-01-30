53686b082850493a368bd64d150f521f7f75c683
Hector Rojas Barcenas <hectorrojasbarcenas@MacBook-Air-de-Hector.local>
2023-01-29 22:43:36 -0600

    Add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,9 @@
         <!-- Fonts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📌</text></svg>">
+
         @vite(['resources/js/app.js','resources/sass/app.scss'])
-    
-        <meta charset="UTF-8">
-            @if (Auth::check()) 
-                <meta name="user" content="{{ Auth::user()}}">
-                <meta name="user-role" content="{{ Auth::user()->getRoleNames()->first()}}">
-            @endif 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         
     </head>
     <body class="antialiased">
